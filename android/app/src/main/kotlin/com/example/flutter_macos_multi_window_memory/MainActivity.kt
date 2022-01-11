4feaced5d0b8a686d7fa927954dba1a9ff25c2e6
Kevin Brendel <brendel@coneno.com>
2022-01-11 23:09:49 +0100

    Minimal flutter multi-window example for macOS based on default project.

---
+++ android/app/src/main/kotlin/com/example/flutter_macos_multi_window_memory/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_macos_multi_window_memory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Runner/MainFlutterWindow.swift
@@ -0,0 +1,51 @@
+import Cocoa
+import FlutterMacOS
+
+class MainFlutterWindow: NSWindow {
+    override func awakeFromNib() {
+        initializeFlutter()
+        super.awakeFromNib()
+    }
+    
+    public func initializeFlutter() {
+        let flutterViewController = FlutterViewController.init()
+        let windowFrame = self.frame
+        self.contentViewController = flutterViewController
+        self.setFrame(windowFrame, display: true)
+        
+        let methodChannel = FlutterMethodChannel(name: "com.example/multi_window", binaryMessenger: flutterViewController.engine.binaryMessenger)
+        
+        methodChannel.setMethodCallHandler(methodCallHandler)
+        
+        RegisterGeneratedPlugins(registry: flutterViewController)
+    }
+    
+    private func methodCallHandler(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch (call.method) {
+        case "newWindow":
+            newFlutterWindow()
+            result(true)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func newFlutterWindow() {
+        let window = MainFlutterWindow()
+        window.initializeFlutter()
+        window.styleMask = styleMask
+        window.setFrame(frame, display: true)
+        
+        let windowController = NSWindowController()
+        windowController.window = window
+        windowController.showWindow(self)
+    }
+    
+    override func close() {
+//        isReleasedWhenClosed = true // Can cause crash. But reduces NSApp.windows.count
+        if let controller = contentViewController as? FlutterViewController {
+            controller.engine.shutDownEngine()
+        }
+        super.close()
+    }
+}
